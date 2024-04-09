8a1a87b510bc933f2becebed1ad8272eeeed432a
Asttryd Santos Pacheco <asttryd.pacheco@compasso.com.br>
2024-04-09 17:52:37 -0300

    Add: first React Project

---
+++ paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home Page</h1>
+            <img src="https://tse3.mm.bing.net/th/id/OIP.1zpsYRoq83bsqvpzCdsbRAHaE7?w=248&h=180&c=7&r=0&o=5&pid=1.7" alt="" />
+        </>
+    );
+}
+
+export default Home;
